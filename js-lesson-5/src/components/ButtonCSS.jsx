a6597207fc1a45ed4e4aa23f0d6b6ffba90a92c6
Mariia Paraketsova <paraketsova@gmail.com>
2020-12-07 14:05:36 +0100

    initial commit

---
+++ js-lesson-5/src/components/ButtonCSS.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import '../styles/button.css'; // Tell webpack that Button.js uses these styles
+
+export default function ButtonCSS() {
+  
+    return <div className="button-css" />;
+  
+}
